b8ce6c218991bbab42021b43f4570e07599fb584
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-04-01 12:55:29 +0100

    The moment when a user sent an answer is saved

--- lib/echo.rb
+++ lib/echo.rb
@@ -7,4 +7,8 @@ class Echo
   def self.receive
     gets.delete!('/n')
   end
+
+  def self.time_received
+    Time.now.strftime('%Y-%m-%d | %H:%M')
+  end
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -9,4 +9,10 @@ describe Echo do
     allow(Echo).to receive(:gets).and_return('hello, world/n')
     expect(Echo.receive).to eq('hello, world')
   end
+
+  it 'saves the moment an answer has bstrftime("%d/%m/%Y %H:%M")een received' do
+    time_now = Time.new(2019, 1, 2, 3, 4, 5, '+00:00')
+    allow(Time).to receive(:now).and_return(time_now)
+    expect(Echo.time_received).to eq('2019-01-02 | 03:04')
+  end
 end
